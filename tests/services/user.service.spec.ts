1e5bc2fb1477e6de0bdd1c77d5aa53ec16b73f40
Sternenwarte88 <s.loecher.slc@gmail.com>
2025-12-01 00:52:59 +0100

    added getUser tests

--- tests/services/user.service.spec.ts
+++ tests/services/user.service.spec.ts
@@ -1,5 +1,8 @@
 import fs from 'fs';
 import { beforeEach, describe, expect, test, vi } from 'vitest';
+import IUser from '../../src/models/user.model';
+import { UserFile } from '../../src/models/userFile.model';
+import { Role } from '../../src/types/role';
 import fileProcessor from '../../src/utils/fileProcessor';
 import { UserService } from './../../src/services/user.service';
 
@@ -24,6 +27,17 @@ beforeEach(() => {
     mockedFS.existsSync.mockReturnValue(true);
 });
 
+function createFakeData(): IUser {
+    return {
+        createdAt: new Date(),
+        email: 'test@test.de',
+        id: '123',
+        name: 'test',
+        Role: Role.Guest,
+        updateAt: new Date(),
+    };
+}
+
 describe('Init userService', () => {
     test("File isn't avaible", () => {
         mockedFS.existsSync.mockReturnValue(false);
@@ -36,5 +50,44 @@ describe('Init userService', () => {
         const userService = new UserService();
 
         expect(mockedFileProcessor.writeFile).not.toBeCalled();
+    });
+});
+
+describe('Get User', () => {
+    test('Failing getting User', async () => {
+        const fakeData = createFakeData();
+        const fakeUserFile: UserFile = {
+            users: [fakeData],
+        };
+        const parsedFakedUserFile = JSON.stringify(fakeUserFile);
+
+        mockedFileProcessor.getCompleteData.mockResolvedValue(
+            parsedFakedUserFile,
+        );
+
+        const userService = new UserService();
+
+        await expect(userService.getUser('345')).rejects.toThrowError(
+            `User with id 345 not found`,
+        );
+    });
+
+    test('Getting User', async () => {
+        const fakeData = createFakeData();
+        const fakeUserFile: UserFile = {
+            users: [fakeData],
+        };
+        const parsedFakedUserFile = JSON.stringify(fakeUserFile);
+
+        mockedFileProcessor.getCompleteData.mockResolvedValue(
+            parsedFakedUserFile,
+        );
+
+        const userService = new UserService();
+        const result = await userService.getUser(fakeData.id);
+
+        expect(result.createdAt).toBeInstanceOf(Date);
+        expect(typeof result.id).toBe('string');
+        expect(Object.values(Role)).toContain(result.Role);
     });
 });
