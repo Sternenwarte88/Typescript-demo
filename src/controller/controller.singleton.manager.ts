7695e375c060590992c2eade96f35e784feb049a
Sternenwarte88 <s.loecher.slc@gmail.com>
2025-12-08 00:14:55 +0100

    Added comments to user.controller.ts

--- src/controller/controller.singleton.manager.ts
+++ src/controller/controller.singleton.manager.ts
@@ -1,5 +1,11 @@
 import { CourseController } from './course.controller.js';
 import { UserController } from './user.controller.js';
 
+/**
+ * The exported singleton for CourseController
+ */
 export const courseController = new CourseController();
+/**
+ * The exported singleton for UserController
+ */
 export const userController = new UserController();
--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -6,7 +6,10 @@ import { userService } from '../services/service.singleton.manager.js';
 
 export class UserController {
     /**
-     * name
+     * This method extracts the ID from the query and provides it to the GetUser method from UserService
+     * @param req The Id which User should be looked up
+     * @param res Sends back the result
+     * @returns Either it breaks out with an error or it returns nothing
      */
     public async GetUser(req: Request, res: Response): Promise<void> {
         const id = req.query.id;
@@ -24,12 +27,23 @@ export class UserController {
         res.status(200).json(user);
     }
 
+    /**
+     * This method calls the userService get users and sends the users object back
+     * @param _req not used at all
+     * @param res The response with status and user object as JSON
+     */
     public async GetUsers(_req: Request, res: Response): Promise<void> {
         const users = await userService.getUsers();
 
         res.status(200).json(users);
     }
 
+    /**
+     * This methid extracts the informations from the request body and provide it to the Service
+     * @param req The User data
+     * @param res the response to the client
+     * @returns Either nothing or it breaks out with an error
+     */
     public async UpdateUser(req: Request, res: Response): Promise<void> {
         const user = plainToInstance(User, req.body);
 
@@ -49,6 +63,12 @@ export class UserController {
         res.status(200).send({ msg: 'ok' });
     }
 
+    /**
+     * This method provides the ID from the query to the Service, for deleting the user.
+     * @param req The ID which user should be deleted
+     * @param res the response back to the client
+     * @returns Either it returns nothing or it breaks out with an error
+     */
     public async DeleteUser(req: Request, res: Response): Promise<void> {
         const id = req.query.id;
 
@@ -64,6 +84,12 @@ export class UserController {
         res.status(200).send({ msg: 'ok' });
     }
 
+    /**
+     * This method extracts the data and provide it to the userService for creating a new user
+     * @param req The information for making a new user
+     * @param res the respons to the client
+     * @returns Either nothing oder it breaks out with an error
+     */
     public async CreateUser(req: Request, res: Response): Promise<void> {
         const user = plainToInstance(User, req.body);
 
